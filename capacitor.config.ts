8df113f452e19e8b04f52ad4170f44709e559a57
chutipong1998 <chutipong.sa.60@ubu.ac.th>
2022-12-27 09:54:57 +0700

    inital project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'what_am_i',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
